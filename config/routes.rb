0cab3d27808c7949f15fad83ff8a11bd39d0e0b4
github username <github email address>
2018-08-01 15:49:20 -0400

    styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :users
   resources :categories
   resources :posts
+
+  get 'about', to: 'static#about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
